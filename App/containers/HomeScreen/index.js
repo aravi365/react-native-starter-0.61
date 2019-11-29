0d7703f724f5b5969868ee02290a0009f917d7ce
Aravind <aravind.m@cubettech.com>
2019-11-29 16:56:14 +0530

    adds snackbar for no interet message

--- App/containers/HomeScreen/index.js
+++ App/containers/HomeScreen/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {View, Button, Text, Input, Icon} from 'native-base';
 import {TextInput, StyleSheet, TouchableOpacity} from 'react-native';
 import {NetworkContext} from '../../components/NetworkProvider';
+import SnackBar from 'react-native-snackbar-component';
 export default class HomeScreen extends React.Component {
   static contextType = NetworkContext;
   render() {
@@ -11,7 +12,18 @@ export default class HomeScreen extends React.Component {
           backgroundColor: 'red',
           justifyContent: 'center',
           alignItems: 'center',
+          marginTop: 100,
         }}>
+        <SnackBar
+          backgroundColor="red"
+          visible={!this.context.isConnected}
+          textMessage="No Active Internet connection!"
+          // actionHandler={() => {
+          //   console.log('snackbar button clicked!');
+          // }}
+          actionText=" "
+          autoHidingTime={10}
+        />
         <Text style={{fontSize: 20}}>Hello this is a test message</Text>
         <Text>
           You are now {this.context.isConnected ? 'online' : 'offline'}
